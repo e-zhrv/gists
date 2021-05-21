df912e1a27e988013db0e348b2251c05a6283f0b
Evgeniy <e-zhrv@users.noreply.github.com>
2021-05-21 12:16:14 +0300

    Update transactions_to_send.sql

--- transactions_to_send.sql
+++ transactions_to_send.sql
@@ -1,14 +1,14 @@
 SELECT 
-  RDate, 
+	RDate, 
   Phone, 
   sum(Sum) as Sum, 
   sum(SumRefund) as SumRefund, 
   max(Date) as maxTime 
 FROM 
-  ga_1c_matching.ga_stored_matched_payments_and_clientids 
-WHERE
-  ImportToGA_Status = '0' AND 
-  _ga is not null 
+	ga_1c_matching.ga_stored_matched_payments_and_clientids 
+WHERE 
+	ImportToGA_Status = '0' 
+    AND ((ClientIDs_json LIKE '%_ga":null%' AND ClientIDs_json LIKE '%_ga":"GA%') OR  ClientIDs_json NOT LIKE '%_ga":null%') 
 GROUP BY 
   Phone, 
   RDate;
